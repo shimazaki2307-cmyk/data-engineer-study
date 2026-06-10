5910170fbabdc76d993ebac7a16514d044e91311
akira <shimazaki2307@gmail.com>
2026-06-10 22:55:48 +0900

    combine functions practice

--- sqlite_test.py
+++ sqlite_test.py
@@ -4,10 +4,7 @@ def get_average_age():
     conn = sqlite3.connect("sample.db")
     cursor = conn.cursor()
 
-    cursor.execute("""
-    SELECT AVG(age)
-    FROM people
-    """)
+    cursor.execute("SELECT AVG(age) FROM people")
 
     result = cursor.fetchone()[0]
 
@@ -15,6 +12,17 @@ def get_average_age():
 
     return result
 
-average_age = get_average_age()
+def get_count():
+    conn = sqlite3.connect("sample.db")
+    cursor = conn.cursor()
 
-print("平均年齢：",average_age)
+    cursor.execute("SELECT COUNT(*) FROM people")
+
+    result = cursor.fetchone()[0]
+
+    conn.close()
+
+    return result
+
+print("人数：",get_count())
+print("平均年齢：",get_average_age())
