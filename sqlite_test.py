a362f91a2f428c7a3ba676bccf0772a6a5c7b15e
akira <shimazaki2307@gmail.com>
2026-06-11 22:18:01 +0900

    fetchall practice

--- sqlite_test.py
+++ sqlite_test.py
@@ -1,28 +1,21 @@
 import sqlite3
 
-def get_average_age():
-    conn = sqlite3.connect("sample.db")
+def get_people():
+    conn = sqlite3.connect ("sample.db")
     cursor = conn.cursor()
 
-    cursor.execute("SELECT AVG(age) FROM people")
-
-    result = cursor.fetchone()[0]
-
+    cursor.execute("""
+                   SELECT name,age
+                   FROM people
+                   """)
+    
+    result = cursor.fetchall()
+    
     conn.close()
-
+    
     return result
 
-def get_count():
-    conn = sqlite3.connect("sample.db")
-    cursor = conn.cursor()
+people = get_people()
 
-    cursor.execute("SELECT COUNT(*) FROM people")
-
-    result = cursor.fetchone()[0]
-
-    conn.close()
-
-    return result
-
-print("人数：",get_count())
-print("平均年齢：",get_average_age())
+for person in people:
+    print(person)
