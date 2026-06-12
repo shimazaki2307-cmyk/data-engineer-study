ff2099bf93232f188fa22d1cfbc95176692b8c9a
akira <shimazaki2307@gmail.com>
2026-06-13 00:21:12 +0900

    where practice

--- sqlite_test.py
+++ sqlite_test.py
@@ -19,3 +19,24 @@ people = get_people()
 
 for person in people:
     print(person)
+
+def get_people_over_30():
+    conn = sqlite3.connect("sample.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   SELECT name,age
+                   FROM people
+                   WHERE age >= 30
+                   """)
+    
+    result = cursor.fetchall()
+
+    conn.close()
+
+    return result
+
+people = get_people_over_30()
+
+for person in people:
+    print("名前：",person[0],"年齢：",person[1])
